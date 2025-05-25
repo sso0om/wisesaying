8f82e8f6224db703c9e26670a416db75ff218889
sso0om <ssooom142@gmail.com>
2025-05-25 12:51:28 +0900

    feet: Step2 명언 등록(입력만) 기능 추가

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,6 +15,12 @@ public class App {
 
             if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                sc.nextLine();
+
+                System.out.print("작가 : ");
+                sc.nextLine();
             }
         }
         sc.close();
