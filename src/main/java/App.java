f679b316f6d76dbf6367bbe6054dfcc2bb2a7eec
sso0om <ssooom142@gmail.com>
2025-05-25 14:23:16 +0900

    feat: Step6 명언 삭제 기능 추가

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,33 +15,67 @@ public class App {
 
         while (true) {
             System.out.print("명령) ");
-            String cmd = sc.nextLine();
+            String cmd = sc.nextLine().trim();
 
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String content = sc.nextLine();
+                String content = sc.nextLine().trim();
 
                 System.out.print("작가 : ");
-                String author = sc.nextLine();
+                String author = sc.nextLine().trim();
 
                 WiseSaying wiseSaying = new WiseSaying(content, author);
                 wiseSayingList.add(wiseSaying);
                 System.out.printf("%d번 명언이 등록되었습니다.%n", wiseSaying.getId());
             } else if (cmd.equals("목록")) {
                 printWiseSayingList();
+            } else if (cmd.startsWith("삭제?id=")) {
+                deleteWiseSaying(getCmdId(cmd));
             }
         }
         sc.close();
     }
-
+    
     /**
      * 명언 목록 조회 (system.print)
      */
     private void printWiseSayingList() {
         for (int i = wiseSayingList.size() - 1; i >= 0; i--) {
             wiseSayingList.get(i).printWiseSaying();
+        }
+    }
+
+    /**
+     * 명언 삭제
+     * @param id
+     */
+    private void deleteWiseSaying(long id) {
+        for (int i = 0; i < wiseSayingList.size(); i++) {
+            if (wiseSayingList.get(i).getId() == id) {
+                wiseSayingList.remove(i);
+                System.out.printf("%d번 명언이 삭제되었습니다.%n", id);
+                break;
+            }
+        }
+    }
+
+    /**
+     * 삭제할 명언 id 추출 (삭제?id=)
+     * @param cmd
+     * @return wiseSaying id
+     */
+    private long getCmdId(String cmd) {
+        String[] parts = cmd.split("\\?id=");
+
+        if (parts.length != 2) {
+            return -1;
+        }
+        try {
+            return Long.parseLong(parts[1]);
+        } catch (NumberFormatException e) {
+            return -1;
         }
     }
 }
