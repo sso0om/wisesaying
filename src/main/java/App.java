3c48d0d57a98f49b5743eea32844bd0c65b77daa
sso0om <ssooom142@gmail.com>
2025-05-25 14:59:38 +0900

    feat: Step8 명언 수정 기능 추가

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,6 +25,8 @@ public class App {
                 printWiseSayingList();
             } else if (cmd.startsWith("삭제?id=")) {
                 deleteWiseSaying(getCmdId(cmd));
+            } else if (cmd.startsWith("수정?id=")) {
+                modifyWiseSaying(getCmdId(cmd));
             }
         }
         sc.close();
@@ -55,31 +57,62 @@ public class App {
     }
 
     /**
-     * 명언 삭제
-     * @param id
+     * 명언 수정
+     * @param cmdId
      */
-    private void deleteWiseSaying(long id) {
-        if (id == -1) {
+    private void modifyWiseSaying(long cmdId) {
+        if (cmdId == -1) {
             return;
         }
 
         boolean hasId = false;
         for (int i = 0; i < wiseSayingList.size(); i++) {
-            if (wiseSayingList.get(i).getId() == id) {
+            if (wiseSayingList.get(i).getId() == cmdId) {
                 hasId = true;
-                wiseSayingList.remove(i);
-                System.out.printf("%d번 명언이 삭제되었습니다.%n", id);
+                WiseSaying wiseSaying = wiseSayingList.get(i);
+
+                System.out.printf("명언(기존) : %s%n", wiseSaying.getContent());
+                System.out.print("명언 : ");
+                wiseSaying.setContent(sc.nextLine().trim());
+
+                System.out.printf("작가(기존) : %s%n", wiseSaying.getAuthor());
+                System.out.print("작가 : ");
+                wiseSaying.setAuthor(sc.nextLine().trim());
+
                 break;
             }
         }
         if (!hasId) {
-            System.out.printf("%d번 명언은 존재하지 않습니다.%n", id);
+            System.out.printf("%d번 명언은 존재하지 않습니다.%n", cmdId);
+        }
+    }
+
+    /**
+     * 명언 삭제
+     * @param cmdId
+     */
+    private void deleteWiseSaying(long cmdId) {
+        if (cmdId == -1) {
+            return;
+        }
+
+        boolean hasId = false;
+        for (int i = 0; i < wiseSayingList.size(); i++) {
+            if (wiseSayingList.get(i).getId() == cmdId) {
+                hasId = true;
+                wiseSayingList.remove(i);
+                System.out.printf("%d번 명언이 삭제되었습니다.%n", cmdId);
+                break;
+            }
+        }
+        if (!hasId) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.%n", cmdId);
         }
     }
 
 
     /**
-     * 삭제할 명언 id 추출 (삭제?id=)
+     * 수정,삭제할 명언 id 추출 (?id=)
      * @param cmd
      * @return wiseSaying id (오입력 시 -1 처리)
      */
